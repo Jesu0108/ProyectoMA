e8f3eec738488713658bd4bddc4b664a7b96fb9a
jesartrod <jartiguez2@gmail.com>
2021-01-20 11:28:58 +0100

    a

--- ProyectoMA-Eclispse/src/view/FrmListaPerfiles.java
+++ ProyectoMA-Eclispse/src/view/FrmListaPerfiles.java
@@ -1,11 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
